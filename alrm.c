d05236a900f2ad671d78020037d576ec68fcb071
bsankar <balajesankar@outlook.com>
2015-07-22 00:50:48 +0530

    Settimer added. Not working

--- alrm.c
+++ alrm.c
@@ -1,20 +1,34 @@
 #include<stdio.h>
 #include <unistd.h>
 #include<signal.h>
-
+#include<sys/time.h>
 int alarm_stop=0; 
 unsigned int alarm_period=30;
 
+struct itimerval it;
+
+
 void on_alarm(int signal)
 {
-alarm(alarm_period);
+it.it_value.tv_sec=10;
+it.it_value.tv_usec=0;
+it.it_interval.tv_sec=10;
+it.it_interval.tv_usec=0;
+
+setitimer(ITIMER_REAL,&it,NULL);
 printf("Alarm Recieved");
 }
 
 int main()
 {
 signal(SIGALRM,on_alarm);
-alarm(alarm_period);
+it.it_value.tv_sec=10;
+it.it_value.tv_usec=0;
+it.it_interval.tv_sec=10;
+it.it_interval.tv_usec=0;
+
+
+setitimer(ITIMER_REAL,&it,NULL);
 for(;;)
 {
 }
